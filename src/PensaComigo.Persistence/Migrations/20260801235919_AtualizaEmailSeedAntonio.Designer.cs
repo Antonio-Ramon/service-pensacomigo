7b713907cf687bf203db07e3d6c7bb99c7541127
Antonio Ramon <antonio-ramon-dev@outlook.com>
2026-08-01 23:36:25 -0300

    feat(persistence): atualizado email do seed do autor com migration

--- src/PensaComigo.Persistence/Migrations/20260801235919_AtualizaEmailSeedAntonio.Designer.cs
+++ src/PensaComigo.Persistence/Migrations/20260801235919_AtualizaEmailSeedAntonio.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PensaComigo.Persistence;
@@ -11,9 +12,11 @@ using PensaComigo.Persistence;
 namespace PensaComigo.Persistence.Migrations
 {
     [DbContext(typeof(PensaComigoDbContext))]
-    partial class PensaComigoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260801235919_AtualizaEmailSeedAntonio")]
+    partial class AtualizaEmailSeedAntonio
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
